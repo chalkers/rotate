0a0e752c8ea9bb2bd874c612cef65f51cf6c82fd
craigsdennis <craig@teamtreehouse.com>
2015-11-09 22:41:43 -0800

    Better comments for clarity sake

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = function rotate(value) {
     var rotated = value >> 1;
+    // Check to see if the first bit was set...
     if (1 & value) {
-    	rotated += 128;
+    	//...flip the furthest bit on.
+    	rotated += (1 << 7);
     }
     return rotated;
 };
